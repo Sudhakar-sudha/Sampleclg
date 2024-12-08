ce8f648402139081957f1b3b0579aa0792bcea23
sudhakarsudha8672@gmail.com <sudhakarsudha8672@gmail.com>
2024-12-08 20:27:16 +0530

    house works

---
+++ Components/Details.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export function Details() {
+  return (
+    <View>
+      <Text>Details Page</Text> {/* Properly closed the <Text> tag */}
+    </View>
+  );
+}
